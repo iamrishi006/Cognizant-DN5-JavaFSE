f8ddaf0eee213a3de5e3e90216801028142a3699
Rushikesh Yeole <rushiyeole19@gmail.com>
2026-06-28 03:22:27 +0530

    Completed Hands-on 6 - Find Country by Country Code using Spring Data JPA

---
+++ Week2/Spring_Data_JPA_with_Spring_Boot_AND_Hibernate/HandsOn6_FindCountryBasedOnCountryCode/README.md
@@ -0,0 +1,20 @@
+\# Hands-on 6
+
+
+
+Implemented Find Country by Country Code using Spring Data JPA.
+
+
+
+\## Features
+
+\- Custom Exception (CountryNotFoundException)
+
+\- findCountryByCode() Service Method
+
+\- Repository findById()
+
+\- Transaction Management
+
+\- Tested using Spring Boot Application
+
---
+++ Week2/Spring_Data_JPA_with_Spring_Boot_AND_Hibernate/HandsOn6_FindCountryBasedOnCountryCode/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,45 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+	private static final Logger LOGGER =
+			LoggerFactory.getLogger(OrmLearnApplication.class);
+
+	private static CountryService countryService;
+
+	public static void main(String[] args) throws CountryNotFoundException {
+
+		ApplicationContext context =
+				SpringApplication.run(OrmLearnApplication.class, args);
+
+		countryService = context.getBean(CountryService.class);
+
+		LOGGER.info("Inside main");
+
+		testFindCountryByCode();
+	}
+
+	private static void testFindCountryByCode()
+			throws CountryNotFoundException {
+
+		LOGGER.info("Start");
+
+		Country country = countryService.findCountryByCode("IN");
+
+		LOGGER.debug("Country: {}", country);
+
+		LOGGER.info("End");
+	}
+}
